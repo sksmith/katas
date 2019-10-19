84a74a44175a451ac3eb8bb5fdf743a17d012218
Smith, Sean K <Sean.Smith@dcsg.com>
2019-10-19 15:06:29 -0400

    Began working on snail

--- snail/snail.go
+++ snail/snail.go
@@ -2,5 +2,16 @@ package snail
 
 // Snail returns the path a snail would take
 func Snail(area [][]int) (path []int) {
-	return []int{}
+	path = make([]int, len(area)*len(area))
+	lap(area, path, 0, 0)
+	return path
+}
+
+func lap(area [][]int, path []int, pathidx, startx, starty int) {
+	if pathidx >= len(path) {
+		return
+	}
+	for x := 0; x < len(area[starty]); x++ {
+
+	}
 }
