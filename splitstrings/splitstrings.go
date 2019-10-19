3d8eccd718751cda9a3030715f291c3b86c740e5
Sean K Smith <ssmith2347@gmail.com>
2019-10-19 14:26:59 -0400

    Added codewars url to Splitstrings

--- splitstrings/splitstrings.go
+++ splitstrings/splitstrings.go
@@ -2,6 +2,7 @@ package splitstrings
 
 // Splitstrings splits a string into pairs of two characters,
 // if there are an odd number of characters adds a trailing '_'
+// https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/train/go
 func Splitstrings(str string) []string {
 	return []string{}
 }
